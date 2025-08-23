be05f172e563a0300f66c5080df2d962bf33141a
unknown <ksalh8266@gmaol.com>
2025-08-23 10:44:01 +0300

    bonus

--- data/bouns.js
+++ data/bouns.js
@@ -2,10 +2,17 @@
  * Load users from JSON file
  *
  * @param {string} dbFile
+ * 
  *     This is the path to the json file
  */
 function loadUsers(users, dbFile) {
+    if (require('fs').existsSync(dbFile)) {
+const data = require('fs').readFileSync(dbFile, 'utf-8');
+        const parsedUsers = JSON.parse(data);
+        parsedUsers.forEach(user => users.push(user));
 }
+}
+
 
 /**
  * Load tasks from JSON file
@@ -14,6 +21,12 @@ function loadUsers(users, dbFile) {
  *     This is the path to the json file
  */
 function loadTasks(tasks, dbFile) {
+    if (require('fs').existsSync(dbFile)) {
+        const data = require('fs').readFileSync(dbFile, 'utf-8');
+        const parsedTasks = JSON.parse(data);
+        parsedTasks.forEach(task => tasks.push(task));
+    }
+
 }
 
 /**
@@ -23,6 +36,7 @@ function loadTasks(tasks, dbFile) {
  *     This is the path to the json file
  */
 function saveTasks(tasks, dbFile) {
+    require('fs').writeFileSync(dbFile, JSON.stringify(tasks, null, 2));
 }
 
 /**
@@ -32,6 +46,7 @@ function saveTasks(tasks, dbFile) {
  *     This is the path to the json file
  */
 function saveUsers(users, dbFile) {
+    require('fs').writeFileSync(dbFile, JSON.stringify(users, null, 2));
 }
 
 module.exports = {
